d2d33febe485f654e21c71e5cfaeae8ee2e4f8d6
12BSG12 <carlj8335@gmail.com>
2022-07-21 20:15:22 +0500

    fixed header and added top-slider

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,5 @@
 import * as flsFunctions from "./modules/functions.js";
+import "./modules/slick.js";
 
 flsFunctions.isWebp();
 
@@ -11,7 +12,29 @@ let logo = document.querySelector('.header__logo');
 header__burger.onclick = function(){
     header__burger.classList.toggle('active');
     menu.classList.toggle('active');
-    if($(window).width() <= 580){
+    if($(window).width() <= 990){
         logo.classList.toggle('active');
     }
 }
+
+$(function (){
+    $('.top__slider').slick({
+        dots: true,
+        vertical: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        prevArrow: '<button type="button" class="slick-prev"><img src="img/top/prev.svg" alt="arrow left"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="img/top/next.svg" alt="arrow right"></button>',
+        responsive: [{
+            breakpoint: 560,
+            settings: {
+                autoplay: true,
+                autoplaySpeed: 5000,
+                prevArrow: false,
+                nextArrow: false,
+                dots: false
+              }
+            }
+        ]
+    });
+});
