c4d51c0e54866fcd3cc69929ebe685d48ec0c329
12BSG12 <carlj8335@gmail.com>
2022-07-25 13:15:16 +0500

    added block contact

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,6 @@
 import * as flsFunctions from "./modules/functions.js";
 import "./modules/slick.js";
+import "./modules/svg.js";
 
 flsFunctions.isWebp();
 
@@ -18,51 +19,51 @@ header__burger.onclick = function(){
 }
 
 $(function (){
-    $('.top__slider').slick({
-        dots: true,
-        vertical: true,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        prevArrow: '<button type="button" class="slick-prev"><img src="img/top/prev.svg" alt="arrow left"></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="img/top/next.svg" alt="arrow right"></button>',
-        responsive: [{
-            breakpoint: 560,
-            settings: {
-                autoplay: true,
-                autoplaySpeed: 5000,
-                prevArrow: false,
-                nextArrow: false,
-                dots: false
-              }
+  $('.top__slider').slick({
+      dots: true,
+      vertical: true,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      prevArrow: '<button type="button" class="slick-prev"><img src="img/top/prev.svg" alt="arrow left"></button>',
+      nextArrow: '<button type="button" class="slick-next"><img src="img/top/next.svg" alt="arrow right"></button>',
+      responsive: [{
+          breakpoint: 560,
+          settings: {
+              autoplay: true,
+              autoplaySpeed: 5000,
+              prevArrow: false,
+              nextArrow: false,
+              dots: false
             }
-        ]
-    });
+          }
+      ]
+  });
 
-    $('.products__name').slick({
-        slidesToShow: 4,
-        slidesToScroll: 1,
-        vertical: true,
-        centerMode: true,
-        focusOnSelect: true,
-        asNavFor: '.products__content',
-        prevArrow: '<button type="button" class="products-prev"><img src="img/products/prev.svg" alt="arrow left"></button>',
-        nextArrow: '<button type="button" class="products-next"><img src="img/products/next.svg" alt="arrow right"></button>',
-      });
-      $('.products__content').slick({
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        asNavFor: '.products__name',
-        arrows: false,
-        fade: true,
-        responsive: [{
-            breakpoint: 861,
-            settings: {
-                fade: false,
-                arrows: true,
-                prevArrow: '<button type="button" class="products-prev"><img src="img/products/next.svg" alt="arrow left"></button>',
-                nextArrow: '<button type="button" class="products-next"><img src="img/products/prev.svg" alt="arrow right"></button>',
-              }
-            }
-        ]
-      });
+  $('.products__name').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    vertical: true,
+    centerMode: true,
+    focusOnSelect: true,
+    asNavFor: '.products__content',
+    prevArrow: '<button type="button" class="products-prev"><img src="img/products/prev.svg" alt="arrow left"></button>',
+    nextArrow: '<button type="button" class="products-next"><img src="img/products/next.svg" alt="arrow right"></button>',
+  });
+  $('.products__content').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    asNavFor: '.products__name',
+    arrows: false,
+    fade: true,
+    responsive: [{
+        breakpoint: 861,
+        settings: {
+            fade: false,
+            arrows: true,
+            prevArrow: '<button type="button" class="products-prev"><img src="img/products/next.svg" alt="arrow left"></button>',
+            nextArrow: '<button type="button" class="products-next"><img src="img/products/prev.svg" alt="arrow right"></button>',
+          }
+        }
+    ]
+  });
 });
